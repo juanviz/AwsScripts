a167da177481ac9fe250be23eb5617e2fb87207d
Juan Vicente Herrera Ruiz de Alejo <juan.vicente.herrera@gmail.com>
2013-06-03 12:25:02 +0200

    Added config sample file

--- config.py
+++
@@ -1,8 +0,0 @@
-aws_access_key_id='AKIAIDBRH6EEI62PUFFA'
-aws_secret_access_key='RRe3s9t4w4DjT5e0KbaCzqrvr4G3ArAc4y8ACV+4'
-subnetDev = ["subnet-63d1e508",]
-subnetAll = ["subnet-cb9596a0","subnet-6ed1e505","subnet-4d9b9826","subnet-919cecfc", "subnet-5c9ded31", "subnet-63d1e508",]
-subnetStage = ["subnet-919cecfc","subnet-5c9ded31",]
-subnetProd = ["subnet-cb9596a0","subnet-6ed1e505","subnet-4d9b9826",]
-
-
