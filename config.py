4487a0496bb3a362dc6efc781c07ca1c989f8219
Juan Vicente Herrera Ruiz de Alejo <juan.vicente.herrera@gmail.com>
2013-07-29 16:48:48 +0200

    added some comments for undestand the meaning of some variables

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-aws_access_key_id='xxxx'
-aws_secret_access_key='xxxxxxxx'
-subnetDev = ["Dev",]
-subnetAll = ["Dev,Prod, Stage,"]
-subnetStage = ["Stage",]
-subnetProd = ["Prod",]
-exclusions=["i-xxxxxx",]
+aws_access_key_id='xxxxxxxxxxxx'
+aws_secret_access_key='xxxxxxxxxxxxxxxxxxxxxx'
+# These tags have to match with values of a Tag called "Env" in your instances
+TagDev = ["Dev",]
+TagAll = ["Dev,Prod, Stage,"]
+TagStage = ["Stage",]
+TagProd = ["Prod",]
+# If you want to avoid that some instances will be stopped or started by this script
+# set the instance id up in the following array and will not be stopped or started 
+exclusions=["i-xxxxxxxxxxx",]
