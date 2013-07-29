005c392afd23456e2e243b91148f29c1590fa28b
Juan Vicente Herrera Ruiz de Alejo <juan.vicente.herrera@gmail.com>
2013-07-29 16:32:23 +0200

    added some comments in the help messsages

--- ec2-instances.py
+++ ec2-instances.py
@@ -5,6 +5,12 @@
 # sudo pip install boto
 # script that start, stop or list the instances of an environment
 # All of the instances of an environment has to have a tag called "Env" with one of the values defined in the config.py file
+# Example of usage ./ec2-instances.py stop Dev
+# Dev is the value of Tag "Env" in all of the instances that compose dev environment and is defined in config.py
+# as the value of Env Tag for dev instances
+# ./ec2-instances.py list|start|stop Prod|Stage|Dev|All
+# By default is not allowed to stop production env and you can avoid that some instances be stopped or started by the script
+# setting its instance id in "exclusions" array included in config.py
 
 
  
