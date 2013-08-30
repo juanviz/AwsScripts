0d87345e9c7c85467e4f18b00f53d1037a99a13e
Juan Vicente Herrera Ruiz de Alejo <juan.vicente.herrera@gmail.com>
2013-08-30 16:04:09 +0200

    config file updated

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-aws_access_key_id='xxxxxxxxxxxx'
-aws_secret_access_key='xxxxxxxxxxxxxxxxxxxxxx'
+aws_access_key_id     = ''
+aws_secret_access_key = ''
 # These tags have to match with values of a Tag called "Env" in your instances
-TagDev = ["Dev",]
-TagStage = ["Stage",]
-TagProd = ["Prod",]
+TagDev = ["bq-web-dev","bq-web-qa",]
 # This array includes all of the values used in Tag "Env" for list, stop or start the whole platform
-TagAll = ["Dev,Prod, Stage,"]
+TagAll = ["bq-web-dev","bq-web-qa","bq-web-integration","bq-web-prod",]
+TagStage = ["bq-web-integration",]
+TagProd = ["bq-web-prod",]
 # If you want to avoid that some instances will be stopped or started by this script
 # set the instance id up in the following array and will not be stopped or started 
-exclusions=["i-xxxxxxxxxxx",]
+exclusions=["i-51a02a31",]
