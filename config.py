f9afe7570f1a06652fa7f33a243730169aaed6a7
Juan Vicente Herrera Ruiz de Alejo <juan.vicente.herrera@gmail.com>
2013-09-11 18:35:48 +0200

    modified env values

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 aws_access_key_id     = ''
 aws_secret_access_key = ''
 # These tags have to match with values of a Tag called "Env" in your instances
-TagDev = ["bq-web-dev","bq-web-qa",]
+TagDev = ["dev",]
 # This array includes all of the values used in Tag "Env" for list, stop or start the whole platform
-TagAll = ["bq-web-dev","bq-web-qa","bq-web-integration","bq-web-prod",]
-TagStage = ["bq-web-integration",]
-TagProd = ["bq-web-prod",]
+TagAll = ["dev","qa","integration","prod",]
+TagStage = ["integration",]
+TagProd = ["prod",]
 # If you want to avoid that some instances will be stopped or started by this script
 # set the instance id up in the following array and will not be stopped or started 
-exclusions=["i-51a02a31",]
+exclusions=["",]
